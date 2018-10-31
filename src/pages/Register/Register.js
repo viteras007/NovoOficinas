d2c0c09ccbe50cd612cc36f3905d746ccdb52c50
Victor Hugo <viteras10@gmail.com>
2018-10-31 11:47:07 -0300

    register working

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -5,6 +5,44 @@ import React, { Component } from 'react'
 import "../../cssPadrao/cssPadrao.css";
 
 export default class Sobre extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }   
+
+    }
+    onNameChange = (event) => {
+        this.setState({ name: event.target.value})
+    }
+    onEmailChange = (event) => {
+        this.setState({ email: event.target.value})
+    }
+    onPasswordChange = (event) => {
+        this.setState({ password: event.target.value})
+    }
+    onSubmitRegister = () => {
+        fetch('http://localhost:3001/register',{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if (user){
+                    console.log('REGISTROU')
+                    //MUDAR A ROTA, PORQUE USUARIO REGISTROUU
+                }                
+            })
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div>
@@ -12,18 +50,34 @@ export default class Sobre extends Component {
                     <form>
                         <p className="h4 text-center mb-4">Register</p>
                         <label htmlFor="defaultFormRegisterNameEx" className="grey-text">Your name</label>
-                        <input type="text" id="defaultFormRegisterNameEx" className="form-control" />
+                        <input 
+                            type="text" 
+                            id="defaultFormRegisterNameEx" 
+                            className="form-control" 
+                            onChange={this.onNameChange}
+                            />
                         <br />
                         <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">Your email</label>
-                        <input type="email" id="defaultFormRegisterEmailEx" className="form-control" />
-                        <br />
-                        <label htmlFor="defaultFormRegisterConfirmEx" className="grey-text">Confirm your email</label>
-                        <input type="email" id="defaultFormRegisterConfirmEx" className="form-control" />
+                        <input 
+                            type="email" 
+                            id="defaultFormRegisterEmailEx" 
+                            className="form-control"
+                            onChange={this.onEmailChange}
+                            />
                         <br />
                         <label htmlFor="defaultFormRegisterPasswordEx" className="grey-text">Your password</label>
-                        <input type="password" id="defaultFormRegisterPasswordEx" className="form-control" />
+                        <input 
+                            type="password" 
+                            id="defaultFormRegisterPasswordEx" 
+                            className="form-control" 
+                            onChange={this.onPasswordChange}
+                            />
                         <div className="text-center mt-5 recuoBotao">
-                            <button className="btn btn-unique buttonRegister" type="submit">Register</button>
+                            <button 
+                                className="btn btn-unique buttonRegister" 
+                                type="submit"
+                                onClick={this.onSubmitRegister}
+                            >Register</button>
                         </div>
                     </form>
                 </div>
