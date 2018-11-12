febb12fbf8e917c012b7aea9bca35e9ffc82f1c6
Victor Hugo <viteras10@gmail.com>
2018-11-12 19:01:25 -0200

    added grafico desempenho

--- src/components/GraficoDesempenho/GraficoDesempenho.js
+++ src/components/GraficoDesempenho/GraficoDesempenho.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react';
-import {Chart} from 'primereact/chart';
+import React, { Component } from 'react';
+import { Chart } from 'primereact/chart';
 
 export default class LineChartDemo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            calorias: 0,
+            progressopeso: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/progressouser', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                idusuario: JSON.parse(sessionStorage.getItem('user')).Id,
+            })
+        })
+        .then(response => response.json())
+        .then(progresso => { 
+
+            for(let i = 0; i < progresso.lenght; i++){
+                this.state.progressopeso.push(progresso[i]);
+            }
+            /*
+            this.state.progressopeso.push(progresso[0])
+            this.state.progressopeso.push(progresso[1])
+            this.state.progressopeso.push(progresso[2])*/
+        })
+        console.log(this.state.progressopeso)
+    }
 
     render() {
+
+
+
         const data = {
             labels: ['01/02', '02/03', '20/04', '05/05', '15/06', '17/07', '02/08', '22/08', '12/10', '30/11'],
             datasets: [
@@ -14,7 +46,7 @@ export default class LineChartDemo extends Component {
                     backgroundColor: '#42A5F5',
                     borderColor: '#42A5F5'
                 }
-            ]   
+            ]
         };
 
         return (
