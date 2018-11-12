50411b7fe9db58a311b047615971c27061f1d950
Victor Hugo <viteras10@gmail.com>
2018-11-12 14:39:17 -0200

    alterar dados user

--- src/pages/AlterarMeta/AlterarMeta.js
+++ src/pages/AlterarMeta/AlterarMeta.js
@@ -2,8 +2,8 @@
 import React, { Component } from 'react';
 
 // Component
-import {RadioButton} from 'primereact/radiobutton';
-import {Panel} from 'primereact/panel';
+import { RadioButton } from 'primereact/radiobutton';
+import { Panel } from 'primereact/panel';
 
 //CSS
 import '../../pages/AlterarMeta/AlterarMeta.css'
@@ -11,38 +11,84 @@ import '../../pages/AlterarMeta/AlterarMeta.css'
 
 export default class AlterarMeta extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            valorMetaAtual:'',
-            valorProximaMeta:'',
+            Altura: '',
+            Peso: '',
+            Meta: '',
+            Idade: ''
         }
-      }
-  render() {
-    return (
-        <div>
-        <div  className="conteudo container containerprincipal">
-            <form>
-                <p className="h4 text-center mb-4 titulo">Alterar Meta</p>
-                <label htmlFor="defaultFormRegisterNameEx" className="grey-text">Meta Atual</label>
-                <input type="text" id="MetaAtual"  className="form-control" value="eee" readOnly />
-                <Panel header="Qual seu Peso Atual em Kg?" className="espacamento">
-                    <input type="text" id="PesoAtual"  className="form-control" placeholder="Peso Atual" />
-                </Panel>
-                <Panel header="Qual sua Altura Atual em cm?" className="espacamento">
-                    <input type="text" id="AlturaAtual"  className="form-control" placeholder="Altura Atual"  />
-                </Panel>
-                <Panel header="Meta a atingir em Kg" className="espacamento">
-                    <input type="text" id="AlturaAtual"  className="form-control" placeholder="Altura Atual"  />
-                </Panel>
-                
-                <div className="text-center mt-5 ">
-                        <button className="espacamento btn btn-unique botaoAlterar" type="submit" onClick={() => {
-                        alert("Alteração feita com sucesso!") }}>Confirmar Alteração</button>
+    }
+
+    onSubmitAltera = () => {
+        fetch('http://localhost:3001/alterauser', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: JSON.parse(sessionStorage.getItem('user')).Id,
+                height: this.state.Altura,
+                weight: this.state.Peso,
+                goal: this.state.Meta,
+                idade: this.state.Idade
+            })
+        })
+            .then(response => response.json())
+            .then(alert('ALTERADO COM SUCESSO'))
+
+    }
+    render() {
+        return (
+            <div>
+                <div className="conteudo container containerprincipal">
+                    <form>
+                        <p className="h4 text-center mb-4 titulo">Alterar Meta</p>
+                        <label htmlFor="defaultFormRegisterNameEx" className="grey-text">Meta Atual</label>
+                        <input
+                            type="text"
+                            id="MetaAtual"
+                            className="form-control"
+                            value="eee" readOnly
+                        />
+                        <Panel header="Qual seu Peso Atual em Kg?" className="espacamento">
+                            <input
+                                type="text"
+                                id="PesoAtual"
+                                className="form-control"
+                                placeholder="Peso Atual"
+                                onChange={(event) => { this.setState({ Peso: event.target.value }) }}
+                            />
+                        </Panel>
+                        <Panel header="Qual sua Altura Atual em cm?" className="espacamento">
+                            <input
+                                type="text"
+                                id="AlturaAtual"
+                                className="form-control"
+                                placeholder="Altura Atual"
+                                onChange={(event) => { this.setState({ Altura: event.target.value }) }}
+                            />
+                        </Panel>
+                        <Panel header="Meta a atingir em Kg" className="espacamento">
+                            <input
+                                type="text"
+                                id="AlturaAtual"
+                                className="form-control"
+                                placeholder="Meta"
+                                onChange={(event) => { this.setState({ Meta: event.target.value }) }}
+                            />
+                        </Panel>
+
+                        <div className="text-center mt-5 ">
+                            <button
+                                className="espacamento btn btn-unique botaoAlterar"
+                                type="submit"
+                                onClick={this.onSubmitAltera}>
+                                Confirmar Alteração
+                        </button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-    </div>
-    )
-  }
+            </div>
+        )
+    }
 }
